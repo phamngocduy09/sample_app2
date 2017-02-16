be1e3d8a9c6d4d80d95be008ebbf3a97f3fb83c9
ngocduy <duyphamngoc09@gmail.com>
2017-02-16 08:56:36 +0700

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+  end
     test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
