b80ef8393a1e377f8f377cccb526d51c8636cdbf
ngocduy <duyphamngoc09@gmail.com>
2017-02-16 15:56:39 +0700

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,4 +21,12 @@ class ActiveSupport::TestCase
                                           password: password,
                                           remember_me: remember_me } }
   end
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
