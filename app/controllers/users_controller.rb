2e85fd20736c8521a39eb8b0d322cfa1dcdde330
ngocduy <duyphamngoc09@gmail.com>
2017-02-17 16:59:50 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     redirect_to root_url and return unless current_user?(@user)
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
