f87d98347003a427a13c66fe61162468b180b81d
ngocduy <duyphamngoc09@gmail.com>
2017-02-17 14:45:10 +0700

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {  
   address: "smtp.gmail.com",
   port: 587,
-  domain: "myapp.herokuapp.com",
+  domain: "herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
